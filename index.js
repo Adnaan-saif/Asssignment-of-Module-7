57f7b16794c561feb132a026a0d8913ea0f064ab
Adnaan Saif <ad.xiii.ad@gmail.com>
2023-09-03 12:47:56 +0600

    short edit

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const router = require('./src/Routes');
 const port = process.env.PORT || 3000;
 
 app.use(router);
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log("Server is running");
 })
