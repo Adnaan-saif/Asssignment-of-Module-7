4dfa788a802612f4b9290b13251a0c48a6fd0dfc
Adnaan Saif <ad.xiii.ad@gmail.com>
2023-09-11 23:37:56 +0600

    first 3 tasks completed

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const express = require('express');
-const app = express( );
+require('dotenv').config();
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const multer = require('multer');
+const jwt = require('jsonwebtoken');
+const mysql = require('mysql');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const mongoSanitize = require('express-mongo-sanitize');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const hpp = require('hpp');
+const validator = require('validator');
+
+
+const app = express( );
 
 app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+app.use(mongoSanitize());
+app.use(helmet());
+app.use(hpp());
+
 module.exports = app;
