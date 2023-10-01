0368d3d681f8e85b57c9a66910546368875f875e
Adnaan Saif <ad.xiii.ad@gmail.com>
2023-10-01 06:49:41 +0600

    edited for new assignment

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 const router = require('./src/Routes');
-const port = require('./config.env') || process.env.RUNNING_PORT || 5500;
+const port = require('./config.env') || process.env.RUNNING_PORT || 8080;
 
 app.use(router);
 app.listen(port, () => {
---
+++ src/Controller/adminController.js
@@ -0,0 +1,42 @@
+const app = require("../../app")
+
+export const Create = app.post('/', async (req, res) => {
+    try {
+        let createData = req.body;
+        res.status(201).json({status:"success",data:createData});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({status: "error", message: "An error occurred"});
+    }
+});
+
+export const Read = app.get('/', async (req, res)=>{
+    try{
+        const getdata = req.query
+        res.status(200).json({status:"success",data:getdata});
+    } catch (error){
+        console.error(error);
+        res.status(500).json({status: "error", message: "An error occurred"});
+    }
+});
+
+export const Update = app.put('/', async(req, res)=>{
+    try{
+        const updateID = req.params.id;
+        const updatedata = req.body
+        res.status(200).json({status:"success",id:updateID+"has been updated",data:updatedata});
+    } catch (error){
+        console.error(error);
+        res.status(500).json({status: "error", message: "An error occurred"});
+    }
+});
+
+export const Delete = app.delete('/', async(req, res)=>{
+    try{
+        const deleteID = req.params.id;
+        res.status(200).json({status:"succss",data:deleteID +"has been deleted"});
+    } catch(error){
+        console.error(error);
+        res.status(500).json({status: "error", message: "An error occurred"});
+    }
+});
